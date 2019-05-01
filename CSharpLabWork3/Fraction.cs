5f3f6a11dc4b7fb00f027c21cf51d9b285bfb0e9
Мирослав Левдиков <deadstronghold@gmail.com>
2019-05-01 19:53:18 +0300

    Add project files.

---
+++ CSharpLabWork3/Fraction.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLabWork3
+{
+    public class Fraction
+    {
+        int Numerator;
+        int Denominator;
+        public Fraction() { Numerator = Denominator = 1; }
+        public Fraction(int n, int d)
+        {
+            this.Numerator = n;
+            this.Denominator = d;
+        }
+
+        public void ShowTheFractionMethod()
+        {
+            Console.WriteLine("Дробь в обыкновенном виде:" + "\n");
+            Console.WriteLine("\0" + Numerator);
+            Console.WriteLine("\0" + "---");
+            Console.WriteLine("\0" + Denominator);
+            Console.WriteLine("\n");
+            Console.WriteLine("Дробь в десятичном виде:" + "\n");
+            Console.WriteLine( "\0" + Math.Round(((double)Numerator / (double)Denominator) , 5));
+        }
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            Fraction result = new Fraction(1 , 1);
+            result.Numerator = (f1.Numerator * f2.Denominator + f1.Denominator * f2.Numerator);
+            result.Denominator = f1.Denominator * f2.Denominator; 
+            return result;
+        }
+
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            Fraction result = new Fraction(1, 1);
+            result.Numerator = (f1.Numerator * f2.Denominator - f1.Denominator * f2.Numerator);
+            result.Denominator = f1.Denominator * f2.Denominator;
+            return result;
+        }
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        { 
+            Fraction result = new Fraction(1, 1);
+            result.Numerator = f1.Numerator * f2.Numerator;
+            result.Denominator = f1.Denominator * f2.Denominator;
+            return result;
+        }
+
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            Fraction result = new Fraction(1, 1);
+            result.Numerator = f1.Numerator / f2.Numerator;
+            result.Denominator = f1.Denominator / f2.Denominator;
+            return result;
+        }
+
+        public static bool operator >(Fraction f1, Fraction f2)
+        {
+            Fraction result1 = new Fraction();
+            Fraction result2 = new Fraction();
+            result1.Denominator = f1.Denominator * f2.Denominator;
+            result2.Denominator = f1.Denominator * f2.Denominator;
+            result1.Numerator = (f1.Numerator * f2.Denominator );
+            result2.Numerator = (f1.Denominator * f2.Numerator );
+            if (result1.Numerator > result2.Numerator)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator <(Fraction f1, Fraction f2)
+        {
+            Fraction result1 = new Fraction();
+            Fraction result2 = new Fraction();
+            result1.Denominator = f1.Denominator * f2.Denominator;
+            result2.Denominator = f1.Denominator * f2.Denominator;
+            result1.Numerator = (f1.Numerator * f2.Denominator);
+            result2.Numerator = (f1.Denominator * f2.Numerator);
+            if (result1.Numerator < result2.Numerator)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ CSharpLabWork3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLabWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction(5, 32);
+            Fraction f2 = new Fraction(12, 17);
+            Fraction f3 = new Fraction(5, 25);
+            Fraction f4 = new Fraction(10, 25);
+            Fraction f1plusf2;  //дробь 1 - дробь 2
+            f1plusf2 = f1 + f2;
+            Fraction f3minusf1; //дробь 3 - дробь 1
+            f3minusf1 = f3 - f2;
+
+            Fraction f4plusf2;  //дробь 4 + дробь 2
+            f4plusf2 = f4 + f2;
+            Fraction f1minusf3; //дробь 1 - дробь 3
+            f1minusf3 = f1 - f3;
+
+            Fraction multfraction;
+            multfraction = f1plusf2 * f3minusf1;
+            Fraction divfraction;
+            divfraction = f4plusf2 / f1minusf3;
+
+            if (multfraction > divfraction)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+            Console.WriteLine("\n");
+            if (f1 < f2)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+
+
+            // multfraction.ShowTheFractionMethod();
+
+            f1plusf2.ShowTheFractionMethod();
+            Console.ReadKey();
+        }
+    }
+}
